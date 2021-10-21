97c7245045efa82a0b36cca83adb7b6213be934d
Korn <thanakorn.wongs@ku.th>
2021-10-21 23:04:57 +0700

    add Spring Security with OAuth Login

--- src/main/java/th/ac/ku/kinkao/controller/HomeController.java
+++ src/main/java/th/ac/ku/kinkao/controller/HomeController.java
@@ -1,5 +1,7 @@
 package th.ac.ku.kinkao.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public String getHomePage(Model model) {
+    public String getHomePage(Model model,
+                              @AuthenticationPrincipal OAuth2User principal) {
         model.addAttribute("greeting", "Sawaddee");
+        if (principal != null)
+            model.addAttribute("user", principal.getAttribute("name"));
+        else
+            model.addAttribute("user", "Guest");
+
         // มันจะ return home.html มาให้เรา
         return "home";
     }
+
 }
